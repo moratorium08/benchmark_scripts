8e5a4228b262068e7906bd923935dc6949f94d2e
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2024-06-05 18:17:08 +0900

    Add eldarica

---
+++ bench2/bench/eldarica.py
@@ -0,0 +1,19 @@
+from katsura_bench import *
+
+class Eldarica(Benchmarker):
+    def pre_cmd(self):
+        return 'echo Eldarica'
+    
+    def gen_cmd(self, file: str):
+        return f'eld -portfolio {file}'
+      
+    def parse_stdout(self, stdout):
+      result_data = dict()
+      result_data['result'] = 'invalid' if 'unsat' in stdout else 'valid' if 'sat' in stdout else 'fail' 
+      return result_data
+    
+    def base_dir(self):
+        return '/home/katsura/github.com/moratorium08/hopdr/hopdr/benchmark'
+
+
+do_bench(Hoice())
