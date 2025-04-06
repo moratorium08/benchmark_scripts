fbc56804254021b75f80e8bdc30301787ea25fc8
Hiroyuki Katsura <moratorium08@gmail.com>
2025-04-06 23:47:47 +0100

    Find instance

---
+++ aggregate/find_instance.py
@@ -0,0 +1,50 @@
+import os
+import argparse
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+bench_dir = os.path.join(script_dir, "../benchmark")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--benchmark', nargs='?', default=bench_dir,
+                    help='Benchmark directory')
+parser.add_argument('filename', metavar='filename',
+                    help='filename')
+
+args = parser.parse_args()
+
+with open(args.filename, "r") as f:
+  target = f.read()
+
+
+inputs = os.path.join(bench_dir, "inputs")
+
+found = False
+def find_file():
+    for root, _, files in os.walk(inputs):
+        for fname in files:
+            path = os.path.join(root, fname)
+            try:
+                with open(path, "r") as f:
+                    content = f.read()
+                if content == target:
+                    return os.path.relpath(path, inputs)
+            except Exception as e:
+                print(f"Error reading {path}: {e}")
+    return None
+
+filename = find_file()
+if filename is None:
+    print("unknown file")
+    os.exit(-1)
+
+lists = os.path.join(bench_dir, "lists")
+for dirname in os.listdir(lists):
+    if dirname == "all":
+        continue
+    d = os.path.join(lists, dirname)
+    with open(d, "r") as f:
+        s = f.read()
+    if filename in s:
+        print(dirname)
+
+
